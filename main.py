d1605d019d619ba073e2d92f2954dff25fa890e0
Denis Buzin <buzin.den@bk.ru>
2024-05-10 16:55:51 +0300

    Доработал

--- main.py
+++ main.py
@@ -1,130 +1,139 @@
 import random
+import telebot
 
-from telebot import types, TeleBot, custom_filters
-from telebot.storage import StateMemoryStorage
-from telebot.handler_backends import State, StatesGroup
 from config import TOKEN, USER, PASSWORD, DB_NAME
 from work_db import UseDataBase
 
-print('Start telegram bot...')
-
-state_storage = StateMemoryStorage()
+state_storage = telebot.StateMemoryStorage()
 token_bot = TOKEN
-bot = TeleBot(token_bot, state_storage=state_storage)
+bot = telebot.TeleBot(token_bot, state_storage=state_storage)
 work_db = UseDataBase(USER, PASSWORD, DB_NAME)
-known_users = []
-userStep = {}
 buttons = []
-
-
-def show_hint(*lines):
-    return '\n'.join(lines)
-
-
-def show_target(data):
-    return f"{data['target_word']} -> {data['translate_word']}"
+list_other_word = []
+en_word = None
+ru_word = None
 
 
 class Command:
     ADD_WORD = 'Добавить слово ➕'
-    DELETE_WORD = 'Удалить слово🔙'
-    NEXT = 'Дальше ⏭'
+    DELETE_WORD = 'Удалить слово \U00002716'
+    NEXT = 'Следующее слово ⏭'
+    START = 'Начать \U0001F6A9'
+    END = 'Вернуться в меню 🔙'
 
 
-class MyStates(StatesGroup):
-    target_word = State()
-    translate_word = State()
-    another_words = State()
+def choose_word(user_id):
+    list_word = work_db.get_word(user_id)
+    return random.choice(list_word)
 
 
-def get_user_step(uid):
-    if uid in userStep:
-        return userStep[uid]
-    else:
-        known_users.append(uid)
-        userStep[uid] = 0
-        print("New user detected, who hasn't used \"/start\" yet")
-        return 0
-
-
-@bot.message_handler(commands=['cards', 'start'])
-def create_cards(message):
-    cid = message.chat.id
-    if cid not in known_users:
-        known_users.append(cid)
-        userStep[cid] = 0
-        bot.send_message(cid, "Hello, stranger, let study English...")
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-
-    global buttons
+def get_word_for_start(message):
+    global buttons, list_other_word, en_word, ru_word
     buttons = []
-    target_word = 'Peace'  # брать из БД
-    translate = 'Мир'  # брать из БД
-    target_word_btn = types.KeyboardButton(target_word)
-    buttons.append(target_word_btn)
-    others = ['Green', 'White', 'Hello', 'Car']  # брать из БД
-    other_words_btns = [types.KeyboardButton(word) for word in others]
-    buttons.extend(other_words_btns)
+    two_words = choose_word(message.chat.id)
+    en_word = two_words[0]
+    ru_word = two_words[1]
+    list_other_word = [word[0] for word in work_db.get_other_word(message.chat.id, ru_word)]
+    random.shuffle(list_other_word)
+    other_word = [telebot.types.KeyboardButton(word) for word in list_other_word[:3]]
+    buttons.append(telebot.types.KeyboardButton(en_word))
+    buttons.extend(other_word)
     random.shuffle(buttons)
-    next_btn = types.KeyboardButton(Command.NEXT)
-    add_word_btn = types.KeyboardButton(Command.ADD_WORD)
-    delete_word_btn = types.KeyboardButton(Command.DELETE_WORD)
-    buttons.extend([next_btn, add_word_btn, delete_word_btn])
+    keyboardSTART = telebot.types.ReplyKeyboardMarkup(row_width=2)
+    keyboardSTART.row(
+        buttons[0],
+        buttons[1]
+    )
+    keyboardSTART.row(
+        buttons[2],
+        buttons[3]
+    )
+    keyboardSTART.row(
+        telebot.types.KeyboardButton(Command.NEXT)
+    )
+    keyboardSTART.row(
+        telebot.types.KeyboardButton(Command.END)
+    )
+    bot.send_message(message.chat.id, f"Попробуй отгадай слово - {ru_word} 🇷🇺", reply_markup=keyboardSTART)
 
-    markup.add(*buttons)
-
-    greeting = f"Выбери перевод слова:\n🇷🇺 {translate}"
-    bot.send_message(message.chat.id, greeting, reply_markup=markup)
-    bot.set_state(message.from_user.id, MyStates.target_word, message.chat.id)
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        data['target_word'] = target_word
-        data['translate_word'] = translate
-        data['other_words'] = others
+    @bot.message_handler(func=lambda message_new: True, content_types=['text'])
+    def message_reply(message_new):
+        text = message_new.text
+        if text == en_word:
+            bot.send_message(message_new.chat.id, "Отлично\U00002705 \n Давай следующее слово!")
+        else:
+            bot.send_message(message_new.chat.id, "Попробуй еще раз!\U0001F92C")
 
 
+def handle_message(message):
+    if work_db.get_the_word(message.chat.id, message.text.lower()):
+        work_db.del_the_word(message.chat.id, message.text.lower())
+        bot.send_message(message.chat.id, f'Отлично! Слово 🇷🇺{message.text}🇷🇺 - удалено\U00002705',
+                         reply_markup=keyboardMAIN)
+    else:
+        bot.send_message(message.chat.id, 'Такого слова нет в Вашем словаре!\U0001F92C', reply_markup=keyboardMAIN)
+
+
+def add_words(message):
+    try:
+        lst_pair = message.text.split('-')
+        if not work_db.get_the_word(message.chat.id, lst_pair[0].lower()):
+            work_db.add_new_word(lst_pair[1].lower(), lst_pair[0].lower(), message.chat.id)
+            bot.send_message(message.chat.id, f'Отлично! Пара добавлена в словарь\U00002705',
+                             reply_markup=keyboardMAIN)
+        else:
+            bot.send_message(message.chat.id, 'Такое слово уже есть в Вашем словаре!', reply_markup=keyboardMAIN)
+    except:
+        bot.send_message(message.chat.id, 'Неправильный формат ввода!', reply_markup=keyboardMAIN)
+
+
+keyboardMAIN = telebot.types.ReplyKeyboardMarkup(row_width=2)
+keyboardMAIN.row(
+    telebot.types.KeyboardButton(Command.START),
+)
+keyboardMAIN.row(
+    telebot.types.KeyboardButton(Command.ADD_WORD),
+    telebot.types.KeyboardButton(Command.DELETE_WORD)
+)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет! Для того чтобы общаться с ботом используй кнопки\U0001F447',
+                     reply_markup=keyboardMAIN)
+    work_db.create_structure()
+    work_db.add_new_user(message.chat.id, message.chat.first_name)
+
+
+@bot.message_handler(func=lambda message: message.text == Command.END)
+def back_menu(message):
+    global buttons, list_other_word, en_word
+    buttons.clear()
+    list_other_word.clear()
+    bot.send_message(message.chat.id, 'Выбери кнопку:', reply_markup=keyboardMAIN)
+
+
+@bot.message_handler(func=lambda message: message.text == Command.START)
 @bot.message_handler(func=lambda message: message.text == Command.NEXT)
 def next_cards(message):
-    create_cards(message)
+    get_word_for_start(message)
 
 
 @bot.message_handler(func=lambda message: message.text == Command.DELETE_WORD)
 def delete_word(message):
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        print(data['target_word'])  # удалить из БД
+    msg = bot.send_message(message.from_user.id, 'Напиши слово на 🇷🇺Русском🇷🇺 языке которое хочешь удалить',
+                           reply_markup=telebot.types.ReplyKeyboardRemove())
+    bot.register_next_step_handler(msg, handle_message)
 
 
 @bot.message_handler(func=lambda message: message.text == Command.ADD_WORD)
 def add_word(message):
-    cid = message.chat.id
-    userStep[cid] = 1
-    print(message.text)  # сохранить в БД
+    msg = bot.send_message(message.from_user.id, "Напиши слово в формате '🇷🇺слово - перевод' которое хочешь "
+                                                 "добавить в словарь", reply_markup=telebot.types.ReplyKeyboardRemove())
+    bot.register_next_step_handler(msg, add_words)
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def message_reply(message):
-    text = message.text
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-        target_word = data['target_word']
-        if text == target_word:
-            hint = show_target(data)
-            hint_text = ["Отлично!❤", hint]
-            next_btn = types.KeyboardButton(Command.NEXT)
-            add_word_btn = types.KeyboardButton(Command.ADD_WORD)
-            delete_word_btn = types.KeyboardButton(Command.DELETE_WORD)
-            buttons.extend([next_btn, add_word_btn, delete_word_btn])
-            hint = show_hint(*hint_text)
-        else:
-            for btn in buttons:
-                if btn.text == text:
-                    btn.text = text + '❌'
-                    break
-            hint = show_hint("Допущена ошибка!",
-                             f"Попробуй ещё раз вспомнить слово 🇷🇺{data['translate_word']}")
-    markup.add(*buttons)
-    bot.send_message(message.chat.id, hint, reply_markup=markup)
-
-
-bot.add_custom_filter(custom_filters.StateFilter(bot))
-
-bot.infinity_polling(skip_pending=True)
+if __name__ == '__main__':
+    print('Start telegram bot...')
+    print(ru_word, en_word)
+    bot.polling()
--- work_db.py
+++ work_db.py
@@ -3,7 +3,6 @@ import psycopg2 as pc2
 
 class WorkDB:
     """Класс соединения базы данных"""
-
     def __init__(self, user, password, name_db):
         self.cursor = None
         self.connection = None
@@ -45,8 +44,7 @@ class UseDataBase(WorkDB):
             self.connect_db()
 
             sql_query_users = """CREATE TABLE IF NOT EXISTS user_information(
-                                    id SERIAL PRIMARY KEY, 
-                                    user_id INTEGER, 
+                                    user_id INTEGER PRIMARY KEY, 
                                     user_name VARCHAR(50),
                                     UNIQUE(user_id));"""
 
@@ -56,12 +54,11 @@ class UseDataBase(WorkDB):
                                     id SERIAL PRIMARY KEY,
                                     word_en VARCHAR(80),
                                     word_ru VARCHAR(80),
-                                    user_id INTEGER REFERENCES user_information(id));"""
+                                    user_id INTEGER REFERENCES user_information(user_id));"""
 
             self.cursor.execute(sql_query_words)
             self.connection.commit()
             print("[INFO] Tables created successfully.")
-
         except Exception as er:
             print(f"[ERROR] Failed to insert record into publisher table: {er}")
         finally:
@@ -74,9 +71,7 @@ class UseDataBase(WorkDB):
             self.connect_db()
             sql_query_word = """
                         INSERT INTO words(word_en, word_ru, user_id)
-                        VALUES (%s, %s, 
-                        (SELECT id FROM user_information
-                        WHERE user_id = %s));
+                        VALUES (%s, %s, %s);
                     """
             self.cursor.execute(sql_query_word, (word_en, word_ru, user_id))
             self.connection.commit()
@@ -93,7 +88,8 @@ class UseDataBase(WorkDB):
 
             sql_query = """
                 INSERT INTO user_information(user_id, user_name)
-                VALUES (%s, %s);
+                VALUES (%s, %s)
+                ON CONFLICT (user_id) DO NOTHING;
             """
 
             self.cursor.execute(sql_query, (user_id, user_name))
@@ -116,14 +112,70 @@ class UseDataBase(WorkDB):
             sql_query = """
                         DELETE FROM words
                         WHERE word_ru = %s
-                        AND user_id = (SELECT id FROM user_information WHERE user_id = %s);
+                        AND user_id = %s;
                     """
 
             self.cursor.execute(sql_query, (word.lower(), user_id))
             self.connection.commit()
-            print("[INFO] Delite successfully.")
+            print("[INFO] Delite success!")
         except Exception as err:
             print(f"[ERROR] Failed to create tables: {err}")
+        finally:
+            self.close_connection()
+
+    def get_word(self, user_id):
+        """Функция получения всех слов пользователя"""
+        try:
+            self.connect_db()
+
+            sql_query = """
+                            SELECT word_en, word_ru
+                            FROM words
+                            WHERE user_id = %s;
+                        """
+
+            self.cursor.execute(sql_query, (user_id, ))
+            answer = self.cursor.fetchall()
+            return answer
+        except Exception as err:
+            print(f"[ERROR] Failed: {err}")
+        finally:
+            self.close_connection()
+
+    def get_other_word(self, user_id, word):
+        """Функция получения других слов пользователя"""
+        try:
+            self.connect_db()
+            sql_query = """
+                            SELECT DISTINCT word_en
+                            FROM words
+                            WHERE user_id = %s
+                            AND word_ru != %s;
+                        """
+            self.cursor.execute(sql_query, (user_id, word))
+            answer = self.cursor.fetchall()
+            return answer
+        except Exception as err:
+            print(f"[ERROR] Failed: {err}")
+        finally:
+            self.close_connection()
+
+    def get_the_word(self, user_id, word):
+        """Функция получения конкретного слова пользователя"""
+        try:
+            self.connect_db()
+            sql_query = """
+                            SELECT word_en
+                            FROM words
+                            WHERE user_id = %s
+                            AND word_ru = %s;
+                        """
+            self.cursor.execute(sql_query, (user_id, word))
+            answer = self.cursor.fetchall()
+            return answer
+        except Exception as err:
+            print(f"[ERROR] Failed: {err}")
+            return False
         finally:
             self.close_connection()
 
@@ -131,6 +183,6 @@ class UseDataBase(WorkDB):
 if __name__ == '__main__':
     newClass = UseDataBase('postgres', '310535', 'enjoy_english')
     # newClass.create_structure()
-    # newClass.add_new_user("12345", "katya")
-    newClass.del_the_word("1234", "яблоко")
     print(newClass.WORDS_EXAMPLE)
+    print(newClass.get_word('463243140'))
+    print(newClass.get_other_word('463243140', 'собака'))
